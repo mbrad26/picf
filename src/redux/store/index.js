615d9a2ef9daaee773774e22dbf1ce5b97c4a089
mbrad26 <mbrad26@gmail.com>
2020-09-21 18:12:30 +0100

    create redux store and connect to the store

---
+++ src/redux/store/index.js
@@ -0,0 +1,13 @@
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from  'redux';
+
+import rootReducer from '../reducers';
+
+const logger = createLogger();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger)
+);
+
+export default store;
