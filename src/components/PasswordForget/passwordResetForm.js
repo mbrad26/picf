472aea488dfa8eb741af51dd125b5286dcb466af
mbrad26 <mbrad26@gmail.com>
2020-09-28 15:11:24 +0100

    refactor passwordResetForm

--- src/components/PasswordForget/passwordResetForm.js
+++ src/components/PasswordForget/passwordResetForm.js
@@ -1,30 +1,47 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { doResetPasswordRequest } from '../../redux/actions/user';
 
+const INITIAL_STATE = {
+  email: '',
+  error: null,
+};
+
 const PasswordResetForm = () => {
   console.log('PASSWORD_RESET');
-  const [email, setEmail] = useState('');
   const dispatch = useDispatch();
+  const { authError } = useSelector(state => state.userState);
+  const [state, setState] = useState(INITIAL_STATE);
+  const { email, error } = state;
 
   const onChange = event => 
-    setEmail(event.target.value);
+    setState({ ...state, email: event.target.value });
 
   const onSubmit = event => {
     event.preventDefault();
-    dispatch(doResetPasswordRequest(email));
+    dispatch(doResetPasswordRequest(state.email));
+    setState(INITIAL_STATE);
   };
+
+  useEffect(() => {
+    if(authError) {
+      setState(state => ({ ...state, error: authError }));
+    }
+  }, [authError]);
 
   return (
     <form onSubmit={onSubmit}>
       <input 
         type='email'
+        name='email'
         value={email}
         onChange={onChange}
         placeholder='Email'
       />
       <button type='submit'>Reset</button>
+
+      {error && <p>{error.message}</p>}
     </form>
   );
 };
