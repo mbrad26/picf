f78060c420e3724a4db89f50a3c1b5d00865a46b
mbrad26 <mbrad26@gmail.com>
2020-10-07 17:26:12 +0100

    add ImageModal component

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { 
   Image,
+  Button, 
 } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 
 import './style.css';
-import { doUrlRequest } from '../../redux/actions/images';
+import ImageModal from '../Modal';
+import { doSetActiveImage, doUrlRequest } from '../../redux/actions/images';
 
 const INITIAL_STATE = {
   urls: null,
@@ -15,18 +17,20 @@ const INITIAL_STATE = {
 const Gallery = () => {
   console.log('GALLERY: ');
   const dispatch = useDispatch();
-  const { imagesUrls } = useSelector(state => state.imagesState);
+  const { imagesUrls, isOpen } = useSelector(state => state.imagesState);
   const [state, setState] = useState(INITIAL_STATE);
   const { urls } = state;
 
+  const setActiveImage = url => dispatch(doSetActiveImage(url));
+
   useEffect(() => {
-    dispatch(doUrlRequest())
+    dispatch(doUrlRequest());
   }, [dispatch]);
 
   useEffect(() =>  {
     if(imagesUrls) {
-      setState(state => ({ ...state, urls: imagesUrls }))
-    }
+      setState(state => ({ ...state, urls: imagesUrls }));
+    };
   }, [imagesUrls]);
 
   return (
@@ -34,8 +38,13 @@ const Gallery = () => {
       {urls && urls.map(data => 
         <div key={data.url} className='grid-item'>
           <Image src={data.url} alt='img' loading='lazy'/>
+          <Button onClick={() => setActiveImage(data.url)}>
+            Open
+          </Button>
         </div>
       )}
+
+      {isOpen && <ImageModal />}
     </div>
   );
 };
---
+++ src/components/Modal/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { 
+  Modal, 
+  Image,
+  Button,
+ } from 'react-bootstrap';
+
+import { doCloseModal } from '../../redux/actions/images';
+
+const ImageModal = () => {
+  const dispatch = useDispatch();
+  const { isOpen, activeImage } = useSelector(state => state.imagesState);
+
+  const closeModal = () => dispatch(doCloseModal());
+
+  return (
+    <Modal
+      fade='true'
+      centered
+      scrollable
+      show={isOpen}
+      onHide={closeModal}
+    >
+      <Modal.Header closeButton>
+        <Image src={activeImage} alt='image' />
+      </Modal.Header>
+      <Modal.Body>
+        <p>Modal</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={closeModal}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default ImageModal;
