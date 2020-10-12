c3a02694e82922dc936af53c16d20eedfea15f8d
mbrad26 <mbrad26@gmail.com>
2020-10-12 15:15:46 +0100

    nav links update when location pathname changes

--- src/components/Navigation/authLinks.js
+++ src/components/Navigation/authLinks.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, withRouter } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
 
 import SignOut from '../SignOut';
 import * as ROUTES from '../constants/routes';
 
-const AuthLinks = () => 
-  <>
-    <Nav.Link as={Link} to={ROUTES.HOME}>Home</Nav.Link>
-    <Nav.Link as={Link} to={ROUTES.ACCOUNT}>Account</Nav.Link>
-    <Nav.Link as={SignOut}>SignOut</Nav.Link>
-  </>
-export default AuthLinks;
+const AuthLinks = () => {
+  const history = useHistory();
+  const isHome = history.location.pathname === '/home';
+
+  return  (
+    <>
+      {isHome
+        ? <Nav.Link as={Link} to={ROUTES.ACCOUNT}>Account</Nav.Link>
+        : <Nav.Link as={Link} to={ROUTES.HOME}>Home</Nav.Link>
+      }
+      <Nav.Link as={SignOut}>SignOut</Nav.Link>
+    </>
+  );
+};
+
+export default withRouter(AuthLinks);
