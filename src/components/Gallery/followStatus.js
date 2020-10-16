85731a765d77a5029db316aa450b34828f4dff44
mbrad26 <mbrad26@gmail.com>
2020-10-16 16:20:10 +0100

    extract follow functionality to own component

---
+++ src/components/Gallery/followStatus.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import GroupAddSharpIcon from '@material-ui/icons/GroupAddSharp';
+
+import { 
+  doFollowRequest, 
+} from '../../redux/actions/images';
+
+const FollowStatus = ({ username, userUid }) => {
+  const dispatch = useDispatch();
+  const { authUser } = useSelector(state => state.userState);
+
+  const handleFollow = userUid => dispatch(doFollowRequest(userUid));
+
+  return (
+    <>
+      <p>
+        by {username} 
+        {userUid !== authUser.uid && 
+          <span> 
+            <GroupAddSharpIcon className='icon' onClick={handleFollow} />
+          </span>
+        }
+      </p>
+    </>
+  )
+}
+
+export default FollowStatus;
--- src/components/Gallery/overlay.js
+++ src/components/Gallery/overlay.js
@@ -2,41 +2,29 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
-import GroupAddSharpIcon from '@material-ui/icons/GroupAddSharp';
 
 import { 
   doDeleteRequest, 
-  doFollowRequest, 
 } from '../../redux/actions/images';
 import { auth } from 'firebase';
 import LikeStatus from './likeStatus';
+import FollowStatus from './followStatus';
 
 const Overlay = ({ data }) => {
   console.log('OVERLAY');
   const history = useHistory();
   const dispatch = useDispatch();
-  const { likedStatus } = useSelector(state => state.imagesState);
   const { authUser } = useSelector(state => state.userState);
-  const { name, url, likes } = data;
+  const { name, url, likes, username, userUid } = data;
 
   console.log('USER_UID: ', data);
 
   const handleDelete = () => dispatch(doDeleteRequest(name));
 
-  const handleFollow = userUid => dispatch(doFollowRequest(userUid));
-  
   return (
     <div id='overlay'>
       <LikeStatus name={name} url={url} likes={likes} />
-
-      <p>
-        by {data.username} 
-        {data.userUid !== authUser.uid &&
-          <span> 
-            <GroupAddSharpIcon className='icon' onClick={handleFollow} />
-          </span>
-        }
-      </p>
+      <FollowStatus username={username} userUid={userUid} />
 
       {history.location.pathname.includes('/timeline') &&
         <HighlightOffIcon className='icon' onClick={handleDelete} />} 
