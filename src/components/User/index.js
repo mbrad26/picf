f786daf0339dec64cf819e99bbc9c0336c53b9e8
mbrad26 <mbrad26@gmail.com>
2020-10-31 16:12:55 +0000

    update User component with username and avatar

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 const User = (props) => {
   console.log('USER', props);
+  const { followers } = useSelector(state => state.userState);
+  const userUid = props.match.params.uid;
+  const user = followers.filter(user => user.uid === userUid);
+
+  console.log('USER_FOLLOWERS: ', user);
 
   return (
     <>
       <h1 style={{ 'color': 'red' }}>User</h1>
-      <p>{props.match.params.uid}</p>
+      <div>
+        <Image id='avatar' src={user[0].avatarUrl} roundedCircle/>
+        <p>{user[0].username}</p>
+      </div>
+
     </>
   );
 };
