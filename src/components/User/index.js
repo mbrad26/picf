c3d3518e22115bd7a9f89630c918872526b6003e
mbrad26 <mbrad26@gmail.com>
2020-11-02 17:02:35 +0000

    display folower images

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Image } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import './styles.css';
+import Images from '../Gallery/images';
+import { doUrlRequest } from '../../redux/actions/images';
 
 const User = (props) => {
   console.log('USER', props);
+  const dispatch = useDispatch();
   const { followers } = useSelector(state => state.userState);
+  const { imagesData } = useSelector(state => state.imagesState);
   const userUid = props.match.params.uid;
   const user = followers.filter(user => user.uid === userUid);
 
   console.log('USER_FOLLOWERS: ', user);
+
+  useEffect(() => {
+    dispatch(doUrlRequest(`images/${userUid}/timeline`));
+  }, [dispatch, userUid]);
 
   return (
     <div className='user-container'>
@@ -18,6 +26,10 @@ const User = (props) => {
         <div className='user-avatar'>
           <Image id='avatar' src={user[0].avatarUrl} roundedCircle />
           <span> {user[0].username}</span>
+        </div>
+        <hr />
+        <div className='grid-container'>
+          <Images imagesData={imagesData} />
         </div>
       </div>
     </div>
