a585c09486eb59c40fd0e843d932421d0ff85e23
mbrad26 <mbrad26@gmail.com>
2020-11-07 14:36:16 +0000

    add Following snapshot test

--- src/tests/components/Following/index.test.js
+++ src/tests/components/Following/index.test.js
@@ -3,13 +3,13 @@ import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 
 import store from '../../../redux/store';
-import Followers from  '../../../components/Followers/index';
+import Following from  '../../../components/Following/index';
 
-describe('Followers', () => {
+describe('Following', () => {
   it('renders snapshot', () => {
     const { container } = render(
       <Provider store={store}>
-        <Followers />
+        <Following />
       </Provider>
     );
 
