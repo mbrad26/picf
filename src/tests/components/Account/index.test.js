2d6728c7a01320f4497d3565feb239f11d748842
mbrad26 <mbrad26@gmail.com>
2020-11-07 12:33:42 +0000

    add Account snapshot test

--- src/tests/components/Account/index.test.js
+++ src/tests/components/Account/index.test.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
 import store from '../../../redux/store';
-import App from '../../../components/App';
+import Account from  '../../../components/Account';
 
-describe('App', () => {
+describe('Account', () => {
   it('renders snapshot', () => {
     const { container } = render(
-      <Provider store={store}>
-        <App />
+      <Provider store={store} >
+        <MemoryRouter initialEntries={['/account']}>
+          <Account />
+        </MemoryRouter>
       </Provider>
-      );
+    );
 
     expect(container.firstChild).toMatchSnapshot();
   });
