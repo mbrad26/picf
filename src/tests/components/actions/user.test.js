199800e2fbef768831c22c295b6290e0750d5159
mbrad26 <mbrad26@gmail.com>
2020-11-09 11:35:38 +0000

    doSignupRequest test

---
+++ src/tests/components/actions/user.test.js
@@ -0,0 +1,120 @@
+import { 
+  doSignupRequest, 
+} from '../../../redux/actions/user';
+import { 
+  SIGNUP_REQUEST, 
+} from '../../../redux/constants/actionTypes';
+
+describe('doSignupRequest', () => {
+  it('returns the correct object', () => {
+    const credentials = {};
+    const expectedObject = {
+      type: SIGNUP_REQUEST,
+      payload: credentials,
+    };
+
+    expect(doSignupRequest(credentials)).toEqual(expectedObject);
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
+
+describe('', () => {
+  it('returns the correct object', () => {
+
+  });
+});
