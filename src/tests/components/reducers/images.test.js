a935a51629021f464d1482cfce346aa54c14863a
mbrad26 <mbrad26@gmail.com>
2020-11-10 09:51:28 +0000

    imagesReducer upload progress test

--- src/tests/components/reducers/images.test.js
+++ src/tests/components/reducers/images.test.js
@@ -1,12 +1,29 @@
+import { doSetUploadProgress } from '../../../redux/actions/images';
 import { doRequestError } from '../../../redux/actions/user';
 import imagesReducer from '../../../redux/reducers/images';
 
 describe('imagesReducer', () => {
+  const initialState = { 
+    uploadError: null, 
+    overlayError: null, 
+    uploadProgress: '', 
+    imagesData: '', 
+    likedStatus: null, 
+  };
+
   it('returns error message', () => {
-    const state =  { uploadError: null, overlayError: null, uploadProgress: '', imagesData: '', likedStatus: null };
+    const state =  initialState;
     const action = doRequestError({});
     const newState = imagesReducer(state, action);
 
     expect(newState).toEqual({ ...state, uploadError: {}});
+  });
+
+  it('returns upload progress', () => {
+    const state = initialState;
+    const action = doSetUploadProgress({});
+    const newState = imagesReducer(state, action);
+
+    expect(newState).toEqual({ ...state, uploadProgress: {}});
   });
 });
